2078d0bd89d951c68eb0f9f5eac04aa814c90825
ulkimion <101999652+ulkimion@users.noreply.github.com>
2022-11-04 21:15:13 -0300

    cambio

--- registro.php
+++ registro.php
@@ -24,6 +24,60 @@
         </div>
       </nav>
 
+
+      <form action="">
+            <table>
+                <tr>
+                    <td>NOMBRE</td>
+                    <td> <input type="text" placeholder="ingresa tu nombre" name="nombre"></td>
+                </tr>
+                <tr>
+                    <td>APELLIDO</td>
+                    <td> <input type="text" placeholder="ingresa tu apellido" name="apellido"></td>
+                </tr>
+                <tr>
+                    <td>RUT</td>
+                    <td> <input type="text" placeholder="ingresa tu Rut" name="rut"></td>
+                </tr>
+                <tr>
+                    <td>FECHA DE NACIMIENTO</td>
+                    <td> <input type="text" placeholder="ingresa tu fecha de nacimiento" name="fecha de nacimiento"></td>
+                </tr>
+                <tr>
+                    <td>NACIONALIDAD</td>
+                    <td> 
+                        <select name="nacionalidad">
+                            <option value="">Chilena</option>
+                            <option value="">Canadiense</option>
+                            <option value="">Uruguaya</option>
+                        </select>
+                </td>
+                </tr>
+                <tr>
+                    <td>GENERO</td>
+                    <td> <input type="radio" name="genero"> FEMENINO
+                         <input type="radio" name="genero"> MASCULINO
+                         <input type="radio" name="genero"> OTRO
+                </td>
+                </tr>
+                <tr>
+                    <td>CIUDAD DE RESIDENCIA</td>
+                    <td> <input type="text" placeholder="ingresa tu ciudad de residencia" name="residencia"></td>
+                </tr>
+                <tr>
+                    <td>
+                    <label for="foto">Foto:</label>
+                    <input type="file" id="foto" name="foto">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit"  value="ENVIAR" name="enviar"> 
+                    </td>
+                </tr>
+            </table>
+
+
       <footer class="text-center text-white fixed-bottom" style="background-color: #221144;">
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
             Tecnología Multimedia - 2º Semestre 2022 <br>
