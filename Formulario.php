436f8fd91f5f2abb6ed88a6c5eba6ac1daae4e1e
Multinecz <nicolas.cepeda@alumnos.upla.cl>
2022-11-04 21:46:01 -0300

    form y regis

--- Formulario.php
+++ Formulario.php
@@ -1,31 +1,13 @@
-<!DOCTYPE html>
+--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Inicio</title>
+    <title>FORMULARIO</title>
 </head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark"">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
-              <a class="nav-link" href="#">Ingresar Persona</a>
-              <a class="nav-link" href="listausuarios.php">Lista Personas</a>
-              <a class="nav-link" href="estadisticas.html">Estadisticas</a>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-
-      <form action="">
+<body>  
+        <form action="">
             <table>
                 <tr>
                     <td>NOMBRE</td>
@@ -76,16 +58,5 @@
                     </td>
                 </tr>
             </table>
-
-
-      <footer class="text-center text-white fixed-bottom" style="background-color: #221144;">
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            Tecnología Multimedia - 2º Semestre 2022 <br>
-            Benjamin Gonzalez Fredes <br>
-            Nicolas Cepeda Zamorano
-
-        </div>
-      </footer>
-
 </body>
 </html>
