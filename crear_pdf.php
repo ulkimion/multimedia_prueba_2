f5fe3eadbb5fca19f589dbb341a9f0ed38763489
ulkimion <101999652+ulkimion@users.noreply.github.com>
2022-11-05 13:40:05 -0300

    ingreso fpdf

---
+++ crear_pdf.php
@@ -0,0 +1,35 @@
+<?php
+require('fpdf/fpdf.php');
+
+class PDF extends FPDF
+{
+// Page header
+function Header()
+{
+    // Logo
+    $this->Image('logo_upla.png',10,6,30);
+    // Arial bold 15
+    $this->SetFont('Arial','B',15);
+    // Move to the right
+    $this->Cell(80);
+    // Title
+    $this->Cell(30,10,'Perfil',1,0,'C');
+    // Line break
+    $this->Ln(20);
+}
+
+// Page footer
+function Footer()
+{
+    // Position at 1.5 cm from bottom
+    $this->SetY(-15);
+}
+}
+
+// Instanciation of inherited class
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Times','',12);
+$pdf->Output();
+?>
