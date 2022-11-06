f6adc2b710df0a492565e425b8b0b4983e8f649d
ulkimion <101999652+ulkimion@users.noreply.github.com>
2022-11-05 21:35:06 -0300

    Update estadisticas.php

--- estadisticas.php
+++ estadisticas.php
@@ -18,11 +18,55 @@ $canada = mysqli_fetch_array($Canadiense);
 
 //para tabla de edades
 
-$diez = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento between '#2022/11/06#' and '#2012/11/06#'") or
+$diez = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '2022/11/06' and fecha_nacimiento > '2012/11/06'") or
 die("Problemas en el select:" . mysqli_error($conexion));
 $x = mysqli_fetch_array($diez);
-echo "<br>menores de 10 :" . $x['cantidad'];
+//echo "<br>menores de 10 :" . $x['cantidad'];
 
+$veinte = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '2012/11/06' and fecha_nacimiento > '2002/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$xx = mysqli_fetch_array($veinte);
+//echo "<br>rango 20:" . $xx['cantidad'];
+
+$treinta = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '2002/11/06' and fecha_nacimiento > '1992/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$xxx = mysqli_fetch_array($treinta);
+//echo "<br>rango 30:" . $xxx['cantidad'];
+
+$cuarenta = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '1992/11/06' and fecha_nacimiento > '1982/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$xl = mysqli_fetch_array($cuarenta);
+//echo "<br>rango 40:" . $xl['cantidad'];
+
+$cincuenta = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '1982/11/06' and fecha_nacimiento > '1972/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$l = mysqli_fetch_array($cincuenta);
+//echo "<br>rango 50:" . $l['cantidad'];
+
+$sesenta = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '1972/11/06' and fecha_nacimiento > '1962/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$lx = mysqli_fetch_array($sesenta);
+//echo "<br>rango 60:" . $lx['cantidad'];
+
+$setenta = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '1962/11/06' and fecha_nacimiento > '1952/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$lxx = mysqli_fetch_array($setenta);
+//echo "<br>rango 70:" . $lxx['cantidad'];
+
+$ochenta = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '1952/11/06' and fecha_nacimiento > '1942/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$lxxx = mysqli_fetch_array($ochenta);
+//echo "<br>rango 80:" . $lxxx['cantidad'];
+
+$noventa = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '1942/11/06' and fecha_nacimiento > '1932/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$xc = mysqli_fetch_array($noventa);
+//echo "<br>rango 90:" . $xc['cantidad'];
+
+$cien = mysqli_query($conexion, "select count(*) as cantidad from usuarios where fecha_nacimiento < '1942/11/06' and fecha_nacimiento > '1932/11/06'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$c = mysqli_fetch_array($cien);
+//echo "<br>rango 100 plus:" . $c['cantidad'];
 
 ?>
 
@@ -44,7 +88,7 @@ echo "<br>menores de 10 :" . $x['cantidad'];
 
         var data = google.visualization.arrayToDataTable([
           
-          ['Task', 'Hours per Day'],
+          ['Pais', 'Cantidad'],
           ['Gente Chilena',     <?php echo $chile['cantidad']?>],
           ['Gente Uruguaya',      <?php echo $uruguay['cantidad']?>],
           ['Gente Canadiense',  <?php echo $canada['cantidad']?>],
@@ -61,34 +105,42 @@ echo "<br>menores de 10 :" . $x['cantidad'];
       }
     </script>
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawChart);
+    <script>
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBasic);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses', 'Profit'],
-          ['2014', 1000, 400, 200],
-          ['2015', 1170, 460, 250],
-          ['2016', 660, 1120, 300],
-          ['2017', 1030, 540, 350]
-        ]);
+function drawBasic() {
 
-        var options = {
-          chart: {
-            title: 'Company Performance',
-            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-          },
-          bars: 'horizontal' // Required for Material Bar Charts.
-        };
+      var data = google.visualization.arrayToDataTable([
+        ['Edades', 'Cantidad de gente',],
+        ['Menores de 10', <?php echo $x['cantidad']?>],
+        ['10-20', <?php echo $xx['cantidad']?>],
+        ['20-30', <?php echo $xxx['cantidad']?>],
+        ['30-40', <?php echo $xl['cantidad']?>],
+        ['40-50', <?php echo $l['cantidad']?>],
+        ['50-60', <?php echo $lx['cantidad']?>],
+        ['60-70', <?php echo $lxx['cantidad']?>],
+        ['80-90', <?php echo $xc['cantidad']?>],
+        ['Mayores de 90', <?php echo $c['cantidad']?>],
+      ]);
 
-        var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+      var options = {
+        title: 'Population of Largest U.S. Cities',
+        chartArea: {width: '50%'},
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0
+        },
+        vAxis: {
+          title: 'City'
+        }
+      };
 
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
-    </script>
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
 
+      chart.draw(data, options);
+    }
+</script>
 
 </head>
 <body>
@@ -112,7 +164,8 @@ echo "<br>menores de 10 :" . $x['cantidad'];
       <div id="piechart" style="width: 1500px; height: 900px;"></div>
 
 
-      <div id="barchart_material" style="width: 1200px; height: 500px;"></div>
+      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <div id="chart_div"></div>
 
 
       <footer class="text-center text-white fixed-bottom" style="background-color: #221144;">
