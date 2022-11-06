9b89bdc78a8dfb553bd68ac19d8be77e65e929d1
ulkimion <101999652+ulkimion@users.noreply.github.com>
2022-11-05 23:38:23 -0300

    Ajustes database

--- bd_prueba_2.sql
+++ bd_prueba_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2022 at 03:20 AM
+-- Generation Time: Nov 06, 2022 at 03:37 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nombres` varchar(15) NOT NULL,
-  `apellidos` varchar(15) NOT NULL,
+  `nombres` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
   `nacionalidad` varchar(10) NOT NULL,
   `genero` int(1) NOT NULL,
   `ciudad_residencia` varchar(30) NOT NULL,
@@ -44,12 +44,12 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `nacionalidad`, `genero`, `ciudad_residencia`, `foto`, `fecha_nacimiento`, `rut`) VALUES
-(11, 'NombreA nombreA', 'ApellidoA Apell', 'Chilena', 0, 'Ciudad', 'img/06-11-202203-16-56_7870NombreA nombreA.png', '2022-11-09', 11111),
-(12, 'NombreB nombreB', 'ApellidoB Apell', 'Chilena', 0, 'Ciudad', 'img/06-11-202203-17-28_4068NombreB nombreB.png', '2011-12-09', 22222),
-(13, 'NombreC nombreC', 'ApellidoC Apell', 'Canadiense', 0, 'Ciudad', 'img/06-11-202203-17-53_3398NombreC nombreC.png', '1998-01-09', 33333),
-(14, 'NombreD nombreD', 'ApellidoD Apell', 'Uruguay', 0, 'Ciudad', 'img/06-11-202203-18-19_3230NombreD nombreD.png', '1986-10-09', 44444),
-(15, 'NombreE nombreE', 'ApellidoE Apell', 'Canadiense', 0, 'Ciudad', 'img/06-11-202203-18-45_9026NombreE nombreE.png', '1977-11-09', 55555),
-(16, 'NombreE nombreE', 'ApellidoE Apell', 'Canadiense', 0, 'Ciudad', 'img/06-11-202203-19-29_5234NombreE nombreE.png', '1977-11-09', 55555);
+(11, 'NombreA nombreA', 'ApellidoA ApellidoA', 'Chilena', 0, 'Ciudad', 'img/06-11-202203-16-56_7870NombreA nombreA.png', '2022-11-09', 11111),
+(12, 'NombreB nombreB', 'ApellidoB ApellidoB', 'Chilena', 0, 'Ciudad', 'img/06-11-202203-17-28_4068NombreB nombreB.png', '2011-12-09', 22222),
+(13, 'NombreC nombreC', 'ApellidoC ApellidoC', 'Canadiense', 0, 'Ciudad', 'img/06-11-202203-17-53_3398NombreC nombreC.png', '1998-01-09', 33333),
+(14, 'NombreD nombreD', 'ApellidoD ApellidoD', 'Uruguay', 0, 'Ciudad', 'img/06-11-202203-18-19_3230NombreD nombreD.png', '1986-10-09', 44444),
+(15, 'NombreE nombreE', 'ApellidoE ApellidoE', 'Canadiense', 0, 'Ciudad', 'img/06-11-202203-18-45_9026NombreE nombreE.png', '1977-11-09', 55555),
+(16, 'NombreF nombreF', 'ApellidoF ApellidoF', 'Canadiense', 0, 'Ciudad', 'img/06-11-202203-19-29_5234NombreE nombreE.png', '1977-11-09', 55555);
 
 --
 -- Indexes for dumped tables
